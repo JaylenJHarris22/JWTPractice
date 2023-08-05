60ea2221f51010659e6f4833a2b55cd2e3f15645
Jaylen Harris <harris.j.jaylen22@gmail.com>
2023-08-04 23:57:29 -0500

    created the config file

--- src/main/java/com/example/jwtpractice/config/SecurityConfig.java
+++ src/main/java/com/example/jwtpractice/config/SecurityConfig.java
@@ -1,4 +1,17 @@
 package com.example.jwtpractice.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests
+                .requestMatchers("").permitAll());
+        return http.build();
+    }
 }
